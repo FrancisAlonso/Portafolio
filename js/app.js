8d8547e10fda10cb5c0cca53210ae4fe2cd3aff0
user name <user email>
2023-03-19 02:30:40 -0300

    portar nueva pagina

---
+++ js/app.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){ 
+    $('.tab-a').click(function(){  
+      $(".tab").removeClass('tab-active');
+      $(".tab[data-id='"+$(this).attr('data-id')+"']").addClass("tab-active");
+      $(".tab-a").removeClass('active-a');
+      $(this).parent().find(".tab-a").addClass('active-a');
+     });
+});
+
+
+HTML CSS JSResult Skip Results Iframe
+/* ------------------------------------ Click on login and Sign Up to  changue and view the effect
+---------------------------------------
+*/
+
+function cambiar_login() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+document.querySelector('.cont_form_login').style.display = "block";
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+
+setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
+  
+setTimeout(function(){    
+document.querySelector('.cont_form_sign_up').style.display = "none";
+},200);  
+  }
+
+function cambiar_sign_up(at) {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+document.querySelector('.cont_form_login').style.opacity = "0";
+  
+setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
+},100);  
+
+setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
+},400);  
+
+
+}    
+
+
+
+function ocultar_login_sign_up() {
+
+document.querySelector('.cont_forms').className = "cont_forms";  
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+document.querySelector('.cont_form_login').style.opacity = "0"; 
+
+setTimeout(function(){
+document.querySelector('.cont_form_sign_up').style.display = "none";
+document.querySelector('.cont_form_login').style.display = "none";
+},500);  
+  
+  }
+
+  $(".card").click(function() {
+    var el = $(this),
+      newone = el.clone(true);
+    el.before(newone);
+    $("." + el.attr("class") + ":last").remove();
+  });
+  
+  $(".card2").click(function() {
+    var el = $(this),
+      newone = el.clone(true);
+    el.before(newone);
+    $("." + el.attr("class") + ":last").remove();
+  });
